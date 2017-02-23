d4e96e416162c5691b2afb32316345481ea4899b
xha <xhanrot@idsystemes.com>
2017-02-23 14:13:00 +0100

    Bug in query parameter

--- app/youtube/youtubequery.js
+++ app/youtube/youtubequery.js
@@ -3,21 +3,19 @@ const youtube = require("youtube-api");
 class YoutubeQuery
 {
     querySubscriptions() {
-        return new Promise(
-            function(resolve, reject) {
-                var req = youtube.subscriptions.list({
-                        part: "snippet2",
-                        mine: "true"
-                    }, (err, data) => {
-                        if (err) {
-                            var error = new Error("Error while getting subscriptions");
-                            reject(error);
-                        } else {
-                            resolve(data);
-                        }
-                    });     
-            }
-        )
+        return new Promise((resolve, reject) => {
+            var req = youtube.subscriptions.list({
+                    part: "snippet",
+                    mine: "true"
+                }, (err, data) => {
+                    if (err) {
+                        var error = new Error(err);
+                        reject(error);
+                    } else {
+                        resolve(data);
+                    }
+                });     
+        });
     }
 }
 
