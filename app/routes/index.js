1c7ddc41692eb7adb969f4dd389a980c40818d0b
xha <xhanrot@idsystemes.com>
2017-02-24 10:11:25 +0100

    Refactoring

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,30 +1,30 @@
-var express = require('express');
-var router = express.Router();
-var oauth = require('../youtube/oauthconnection');
-var youtube = require('../youtube/youtubequery');
+var express = require('express')
+var router = express.Router()
+var oauth = require('../youtube/oauthconnection')
+var youtube = require('../youtube/youtubequery')
 
-let oauthConnection = new oauth.OAuthConnection();
+let oauthConnection = new oauth.OAuthConnection()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render("index");
-  oauthConnection.authenticate();
-  oauthConnection.generateUrl();
-});
+router.get('/', function (req, res, next) {
+  res.render('index')
+  oauthConnection.authenticate()
+  oauthConnection.generateUrl()
+})
 
-router.get('/oauthredirect', function(req, res, next) {
+router.get('/oauthredirect', function (req, res, next) {
   oauthConnection.redirect(req, () => {
-    let youtubeQuery = new youtube.YoutubeQuery();
+    let youtubeQuery = new youtube.YoutubeQuery()
     youtubeQuery.querySubscriptions()
                 .then((data) => {
-                  res.end(JSON.stringify(data));
-                  process.exit();
+                  res.end(JSON.stringify(data))
+                  process.exit()
                 })
                 .catch((error) => {
-                  res.end(error.message);
-                  process.exit();
-                });
-    });
-});
+                  res.end(error.message)
+                  process.exit()
+                })
+  })
+})
 
-module.exports = router;
+module.exports = router
--- app/youtube/youtubequery.js
+++ app/youtube/youtubequery.js
@@ -1,22 +1,21 @@
-const youtube = require("youtube-api");
+const youtube = require('youtube-api')
 
-class YoutubeQuery
-{
-    querySubscriptions() {
-        return new Promise((resolve, reject) => {
-            let req = youtube.subscriptions.list({
-                    part: "snippet",
-                    mine: "true"
-                }, (err, data) => {
-                    if (err) {
-                        let error = new Error(err);
-                        reject(error);
-                        return;
-                    }
-                    resolve(data);
-                });     
-        });
-    }
+class YoutubeQuery {
+  querySubscriptions () {
+    return new Promise((resolve, reject) => {
+      youtube.subscriptions.list({
+        part: 'snippet',
+        mine: 'true'
+      }, (err, data) => {
+        if (err) {
+          let error = new Error(err)
+          reject(error)
+          return
+        }
+        resolve(data)
+      })
+    })
+  }
 }
 
-exports.YoutubeQuery = YoutubeQuery;
+exports.YoutubeQuery = YoutubeQuery
