32b15d5202f6559085f7c0eade50cb7b22ece72d
xavier <xhanrot@gmail.com>
2017-08-17 00:27:32 +0200

    test create tags

--- test/server-tests.js
+++ test/server-tests.js
@@ -6,26 +6,23 @@ let should = chai.should()
 let TagRepository = require('../server/repositories/TagRepository')
 let tagRepository = new TagRepository.TagRepository()
 
-let server = require('../server/bin/www')
+let app = require('../server/app')
 
 chai.use(chaiHttp)
 
 describe('Test API', function() {
-  before(() => {
-    models.sequelize.sync()
-  })
-
-  beforeEach(function() {
-    tagRepository.truncate()
+  beforeEach(() => {
+    return models.sequelize.sync()
   })
 
   it('should create tag', function(done) {
-    chai.request(server)
+    chai.request(app)      
       .post('/api/tags/create')
+      .set('Content-Type', 'application/json')
       .send({ "title": "testTag" })
       .end((err, res) => {
         res.should.have.status(200)        
-        res.body.should.be.xml
+        res.body.should.be.json
         res.body.id.should.be.above(1)
         expect(res.body.title).to.equal('testTtg')
         done()
